47c0d668343c1b5f0cb076f2f83f3c046b51f799
Nick <james.nswork@gmail.com>
2023-02-24 22:35:02 +0000

    Implemented checking an authenticated user's points

--- src/models/points.rs
+++ src/models/points.rs
@@ -2,9 +2,23 @@ use serde::Deserialize;
 
 use crate::models::ENDPOINTS;
 use crate::Robolt;
+use crate::utilities::client::Authenticated;
 
 impl<State> Robolt<State> {
     pub fn fetch_points(&self, user_id: u64, universe_id: u64) -> Result<u64, String> {
+        self.request_builder(format!(
+            "{}/v1/universes/{}/users/{}/all-time",
+            ENDPOINTS.points, user_id, universe_id
+        ))
+            .send::<AllTimeScore>()
+            .map(|res| res.all_time_score)
+    }
+}
+
+impl Robolt<Authenticated> {
+    pub fn my_points(&self, universe_id: u64) -> Result<u64, String> {
+        let user_id = self.fetch_current_user()?.id;
+
         self.request_builder(format!(
             "{}/v1/universes/{}/users/{}/all-time",
             ENDPOINTS.points, user_id, universe_id
