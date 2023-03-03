86b2828175035096ec242cb5a064d49ef3bedc86
Nick <james.nswork@gmail.com>
2023-03-03 19:42:32 +0000

    Updated enum visibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub use models::account_information::{AuthenticatedUserSocials, Gender};
+pub use models::account_information::{Gender, SocialsVisibility};
 pub use models::account_settings::{Privacy, PrivacySetting, TradeValue};
 pub use models::friends::FriendshipStatus;
 pub use models::presence::PresenceType;
