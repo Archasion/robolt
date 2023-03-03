2caa00bea7f92002d83823fff0611a328df50148
Nick <james.nswork@gmail.com>
2023-03-03 13:41:58 +0000

    Implemented account settings API

--- src/models/account_settings.rs
+++ src/models/account_settings.rs
@@ -33,6 +33,48 @@ impl Robolt<Authenticated> {
             .function("my_blocked_users")
             .send::<BlockedUsers>()
     }
+
+    pub fn my_email(&self) -> Result<Email, Error> {
+        self.request_builder(format!(
+            "{}/v1/email",
+            ENDPOINTS.account_settings
+        ))
+            .function("my_email")
+            .send::<Email>()
+    }
+
+    pub fn my_trade_value(&self) -> Result<TradeValue, Error> {
+        self.request_builder(format!(
+            "{}/v1/trade-value",
+            ENDPOINTS.account_settings
+        ))
+            .function("my_trade_value")
+            .send::<TradeValueResponse>()
+            .map(|res| res.trade_value)
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct TradeValueResponse {
+    trade_value: TradeValue,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize)]
+pub enum TradeValue {
+    High,
+    Medium,
+    Low,
+    None,
+    Undefined,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Email {
+    email_address: String,
+    verified: bool,
+    can_bypass_password_for_email_update: bool,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
@@ -66,13 +108,13 @@ pub enum PrivacySetting {
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 pub enum Privacy {
-    #[serde(alias = "All")]
-    AllUsers,
+    #[serde(alias = "All", alias = "AllUsers")]
+    Everyone,
     AllAuthenticatedUsers,
-    #[serde(alias = "Followers")]
     FriendsFollowingAndFollowers,
-    #[serde(alias = "Following")]
     FriendsAndFollowing,
+    Followers,
+    Following,
     TopFriends,
     Friends,
     NoOne,
