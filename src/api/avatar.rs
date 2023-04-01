cb3176d9ffb665d7f8093bfe73be1dbc9d4f0ee4
Nick <james.nswork@gmail.com>
2023-04-02 00:04:41 +0100

    Implemented outfit creation

--- src/api/avatar.rs
+++ src/api/avatar.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use crate::api::{Limit, ENDPOINTS};
 use crate::errors::RoboltError;
@@ -73,7 +74,7 @@ impl Robolt<Authenticated> {
 		Ok(())
 	}
 
-	pub fn set_scales(&self, scales: AvatarScales) -> Result<(), RoboltError> {
+	pub fn set_scales(&self, scales: AvatarScale) -> Result<(), RoboltError> {
 		self.request_builder(format!("{}/v1/avatar/set-scales", ENDPOINTS.avatar))
 			.method(Method::POST)
 			.send_body::<_, EmptyResponse>(scales)?;
@@ -99,6 +100,14 @@ impl Robolt<Authenticated> {
 		))
 		.method(Method::POST)
 		.send()
+	}
+
+	pub fn create_outfit(&self, new_outfit: Outfit) -> Result<(), RoboltError> {
+		self.request_builder(format!("{}/v1/outfits/create", ENDPOINTS.avatar))
+			.method(Method::POST)
+			.send_body::<_, EmptyResponse>(new_outfit)?;
+
+		Ok(())
 	}
 }
 
@@ -189,17 +198,30 @@ impl<'a, State> OutfitsFilterBuilder<'a, State> {
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct OutfitDetails {
+	id: u64,
 	#[serde(rename = "playerAvatarType")]
 	avatar_type: AvatarType,
-	#[serde(flatten)]
-	outfit: Outfit,
+	name: String,
+	id_editable: bool,
 	body_colors: BodyColors,
 	assets: Vec<AvatarAsset>,
-	scale: AvatarScales,
+	scale: AvatarScale,
 	outfit_type: OutfitType,
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Outfit {
+	#[serde(rename = "playerAvatarType")]
+	avatar_type: AvatarType,
+	name: &'static str,
+	body_colors: BodyColors,
+	asset_ids: Vec<u64>,
+	scale: AvatarScale,
+	outfit_type: OutfitType,
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub enum OutfitType {
 	Invalid,
 	Avatar,
@@ -232,13 +254,13 @@ pub struct OutfitsFilterBuilder<'a, State> {
 #[serde(rename_all = "camelCase")]
 pub struct OutfitsResponse {
 	pub filtered_count: u32,
-	pub data: Vec<Outfit>,
+	pub data: Vec<PartialOutfit>,
 	pub total: u64,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Outfit {
+pub struct PartialOutfit {
 	pub id: u64,
 	pub name: String,
 	pub is_editable: bool,
@@ -250,7 +272,7 @@ pub struct Avatar {
 	pub player_avatar_type: AvatarType,
 	pub default_shirt_applied: bool,
 	pub default_pants_applied: bool,
-	pub scales: AvatarScales,
+	pub scales: AvatarScale,
 	pub body_colors: BodyColors,
 	pub assets: Vec<AvatarAsset>,
 	pub emotes: Vec<AvatarEmotes>,
@@ -258,7 +280,7 @@ pub struct Avatar {
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct AvatarScales {
+pub struct AvatarScale {
 	pub head: f32,
 	pub depth: f32,
 	pub height: f32,
@@ -270,12 +292,12 @@ pub struct AvatarScales {
 #[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BodyColors {
-	pub head_color_id: u16,
-	pub torso_color_id: u16,
-	pub right_arm_color_id: u16,
-	pub left_arm_color_id: u16,
-	pub right_leg_color_id: u16,
-	pub left_leg_color_id: u16,
+	pub head_color_id: BrickColor,
+	pub torso_color_id: BrickColor,
+	pub right_arm_color_id: BrickColor,
+	pub left_arm_color_id: BrickColor,
+	pub right_leg_color_id: BrickColor,
+	pub left_leg_color_id: BrickColor,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Deserialize)]
@@ -311,8 +333,9 @@ pub struct AvatarAssetMeta {
 	pub version: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize)]
+#[derive(Default, Debug, Clone, PartialEq, Deserialize, Serialize)]
 pub enum AvatarType {
+	#[default]
 	R6 = 1,
 	R15 = 3,
 }
@@ -386,4 +409,218 @@ pub struct UniverseAvatarAssetOverride {
 	#[serde(rename = "assetTypeID")]
 	pub asset_type_id: f32,
 	pub is_player_choice: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize_repr, Serialize_repr)]
+#[repr(u16)]
+pub enum BrickColor {
+	#[default]
+	White = 1,
+	Grey = 2,
+	LightYellow = 3,
+	BrickYellow = 5,
+	LightGreenMint = 6,
+	LightReddishViolet = 9,
+	PastelBlue = 11,
+	LightOrangeBrown = 12,
+	Nougat = 18,
+	BrightRed = 21,
+	MedReddishViolet = 22,
+	BrightBlue = 23,
+	BrightYellow = 24,
+	EarthOrange = 25,
+	Black = 26,
+	DarkGrey = 27,
+	DarkGreen = 28,
+	MediumGreen = 29,
+	LightYellowishOrange = 36,
+	BrightGreen = 37,
+	DarkOrange = 38,
+	LightBluishViolet = 39,
+	Transparent = 40,
+	TrRed = 41,
+	TrLgBlue = 42,
+	TrBlue = 43,
+	TrYellow = 44,
+	LightBlue = 45,
+	TrFluReddishOrange = 47,
+	TrGreen = 48,
+	TrFluGreen = 49,
+	PhosphWhite = 50,
+	LightRed = 100,
+	MediumRed = 101,
+	MediumBlue = 102,
+	LightGrey = 103,
+	BrightViolet = 104,
+	BrYellowishOrange = 105,
+	BrightOrange = 106,
+	BrightBluishGreen = 107,
+	EarthYellow = 108,
+	BrightBluishViolet = 110,
+	TrBrown = 111,
+	MediumBluishViolet = 112,
+	TrMediReddishViolet = 113,
+	MedYellowishGreen = 115,
+	MedBluishGreen = 116,
+	LightBluishGreen = 118,
+	BrYellowishGreen = 119,
+	LigYellowishGreen = 120,
+	MedYellowishOrange = 121,
+	BrReddishOrange = 123,
+	BrightReddishViolet = 124,
+	LightOrange = 125,
+	TrBrightBluishViolet = 126,
+	LightGold = 127,
+	DarkNougat = 128,
+	Silver = 131,
+	NeonOrange = 133,
+	NeonGreen = 134,
+	SandBlue = 135,
+	SandViolet = 136,
+	MediumOrange = 137,
+	SandYellow = 138,
+	EarthBlue = 140,
+	EarthGreen = 141,
+	TrFluBlue = 143,
+	SandBlueMetallic = 145,
+	SandVioletMetallic = 146,
+	SandYellowMetallic = 147,
+	DarkGreyMetallic = 148,
+	BlackMetallic = 149,
+	LightGreyMetallic = 150,
+	SandGreen = 151,
+	SandRed = 153,
+	DarkRed = 154,
+	TrFluYellow = 157,
+	TrFluRed = 158,
+	GunMetallic = 168,
+	RedFlipFlop = 176,
+	YellowFlipFlop = 178,
+	SilverFlipFlop = 179,
+	Curry = 180,
+	FireYellow = 190,
+	FlameYellowishOrange = 191,
+	ReddishBrown = 192,
+	FlameReddishOrange = 193,
+	MediumStoneGrey = 194,
+	RoyalBlue = 195,
+	DarkRoyalBlue = 196,
+	BrightReddishLilac = 198,
+	DarkStoneGrey = 199,
+	LemonMetallic = 200,
+	LightStoneGrey = 208,
+	DarkCurry = 209,
+	FadedGreen = 210,
+	Turquoise = 211,
+	LightRoyalBlue = 212,
+	MediumRoyalBlue = 213,
+	Rust = 216,
+	Brown = 217,
+	ReddishLilac = 218,
+	Lilac = 219,
+	LightLilac = 220,
+	BrightPurple = 221,
+	LightPurple = 222,
+	LightPink = 223,
+	LightBrickYellow = 224,
+	WarmYellowishOrange = 225,
+	CoolYellow = 226,
+	DoveBlue = 232,
+	MediumLilac = 268,
+	SlimeGreen = 301,
+	SmokyGrey = 302,
+	DarkBlue = 303,
+	ParsleyGreen = 304,
+	SteelBlue = 305,
+	StormBlue = 306,
+	Lapis = 307,
+	DarkIndigo = 308,
+	SeaGreen = 309,
+	Shamrock = 310,
+	Fossil = 311,
+	Mulberry = 312,
+	ForestGreen = 313,
+	CadetBlue = 314,
+	ElectricBlue = 315,
+	Eggplant = 316,
+	Moss = 317,
+	Artichoke = 318,
+	SageGreen = 319,
+	GhostGrey = 320,
+	Lilac2 = 321,
+	Plum = 322,
+	Olivine = 323,
+	LaurelGreen = 324,
+	QuillGrey = 325,
+	Crimson = 327,
+	Mint = 328,
+	BabyBlue = 329,
+	CarnationPink = 330,
+	Persimmon = 331,
+	Maroon = 332,
+	Gold = 333,
+	DaisyOrange = 334,
+	Pearl = 335,
+	Fog = 336,
+	Salmon = 337,
+	Terracotta = 338,
+	Cocoa = 339,
+	Wheat = 340,
+	Buttermilk = 341,
+	Mauve = 342,
+	Sunrise = 343,
+	Tawny = 344,
+	Rust2 = 345,
+	Cashmere = 346,
+	Khaki = 347,
+	LilyWhite = 348,
+	Seashell = 349,
+	Burgundy = 350,
+	Cork = 351,
+	Burlap = 352,
+	Beige = 353,
+	Oyster = 354,
+	PineCone = 355,
+	FawnBrown = 356,
+	HurricaneGrey = 357,
+	CloudyGrey = 358,
+	Linen = 359,
+	Copper = 360,
+	DirtBrown = 361,
+	Bronze = 362,
+	Flint = 363,
+	DarkTaupe = 364,
+	BurntSienna = 365,
+	InstitutionalWhite = 1001,
+	MidGray = 1002,
+	ReallyBlack = 1003,
+	ReallyRed = 1004,
+	DeepOrange = 1005,
+	Alder = 1006,
+	DustyRose = 1007,
+	Olive = 1008,
+	NewYeller = 1009,
+	ReallyBlue = 1010,
+	NavyBlue = 1011,
+	DeepBlue = 1012,
+	Cyan = 1013,
+	CGABrown = 1014,
+	Magenta = 1015,
+	Pink = 1016,
+	DeepOrange2 = 1017,
+	Teal = 1018,
+	Toothpaste = 1019,
+	LimeGreen = 1020,
+	Camo = 1021,
+	Grime = 1022,
+	Lavender = 1023,
+	PastelLightBlue = 1024,
+	PastelOrange = 1025,
+	PastelViolet = 1026,
+	PastelBlueGreen = 1027,
+	PastelGreen = 1028,
+	PastelYellow = 1029,
+	PastelBrown = 1030,
+	RoyalPurple = 1031,
+	HotPink = 1032,
 }
